d64028e9fd00b4291af5678a9c8673c78f1a1fa3
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-05-19 17:53:41 -0300

    Added video module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exceptions-filters/prisma.exception-filter';
 import { CatchAllErrorsExceptionFilter } from './exceptions-filters/catch-all-errors.exception-filter';
 import { ValidationPipe } from '@nestjs/common';
+import { InvalidRelationExceptionFilter } from './exceptions-filters/invalid-relation.exception-filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,12 +12,21 @@ async function bootstrap() {
   app.useGlobalFilters(
     new CatchAllErrorsExceptionFilter(httpAdapter),
     new PrismaExceptionFilter(),
+    new InvalidRelationExceptionFilter(),
   );
   app.useGlobalPipes(
     new ValidationPipe({
-      errorHttpStatusCode: 422
+      errorHttpStatusCode: 422,
+      transform: true,
     })
   );
+  const config = new DocumentBuilder()
+    .setTitle("NestJS 10 - Video API")
+    .setDescription("The vídeo API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
 bootstrap();
