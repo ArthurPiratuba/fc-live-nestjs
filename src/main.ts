47fe754dfb6a53bf6106ac13a8998d723944118c
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-05-19 14:42:41 -0300

    Added swc compiler

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exceptions-filters/prisma.exception-filter';
+import { CatchAllErrorsExceptionFilter } from './exceptions-filters/catch-all-errors.exception-filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new PrismaExceptionFilter());
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new CatchAllErrorsExceptionFilter(httpAdapter),
+    new PrismaExceptionFilter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
